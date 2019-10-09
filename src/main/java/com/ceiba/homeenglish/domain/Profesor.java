228bb99f8f280929501ef481e7c8d48eac8efaea
Emel Mora Fernández - Ceiba Software <emel.mora@ceiba.com.co>
2019-10-09 16:28:15 -0500

    ‘guardado_de_los_ultimos_cambios’

--- src/main/java/com/ceiba/homeenglish/domain/Profesor.java
+++ src/main/java/com/ceiba/homeenglish/domain/Profesor.java
@@ -37,53 +37,5 @@ public class Profesor {
 		this.id = id;
 	}
 
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getCedula() {
-		return cedula;
-	}
-
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
 	
 }
