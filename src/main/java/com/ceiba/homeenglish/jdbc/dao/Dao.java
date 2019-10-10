94990b5369138bc461b1bfa7c44ecc3fc97bc751
Emel Mora Fernández - Ceiba Software <emel.mora@ceiba.com.co>
2019-10-10 10:14:46 -0500

    ‘guardado_de_los_ultimos_cambios’

--- src/main/java/com/ceiba/homeenglish/jdbc/dao/Dao.java
+++ src/main/java/com/ceiba/homeenglish/jdbc/dao/Dao.java
@@ -2,16 +2,12 @@ package com.ceiba.homeenglish.jdbc.dao;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.stereotype.Component;
 
 @Component
 public abstract class Dao extends NamedParameterJdbcDaoSupport {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(Dao.class);
 
 	@Autowired
 	private DataSource dataSource;
@@ -22,9 +18,6 @@ public abstract class Dao extends NamedParameterJdbcDaoSupport {
 
 	@Override
 	protected void checkDaoConfig() {
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("--- CheckConfigDao ---");
-		}
 
 		super.setDataSource(this.dataSource);
 	}
