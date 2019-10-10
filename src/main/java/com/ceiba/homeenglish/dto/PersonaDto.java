58d7a018d0db049eb31bfb36844e90c049870685
Emel Mora Fernández - Ceiba Software <emel.mora@ceiba.com.co>
2019-10-10 08:42:36 -0500

    ‘guardado_de_los_ultimos_cambios’

---
+++ src/main/java/com/ceiba/homeenglish/dto/PersonaDto.java
@@ -0,0 +1,65 @@
+package com.ceiba.homeenglish.dto;
+
+public class PersonaDto {
+	
+	protected Long id;
+	protected String cedula;
+	protected String nombre;
+	protected int edad;
+	protected String apellido;
+	protected String direccion;
+	
+	public PersonaDto() {
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+		
+}
--- src/main/java/com/ceiba/homeenglish/dto/ProfesorDto.java
+++ src/main/java/com/ceiba/homeenglish/dto/ProfesorDto.java
@@ -1,73 +1,17 @@
 package com.ceiba.homeenglish.dto;
 
-public class ProfesorDto {
-	
-	private Long id;
-	private String cedula;
-	private String nombre;
-	private int edad;
-	private String apellido;
-	private String direccion;
+public class ProfesorDto  extends PersonaDto{
 	
 	public ProfesorDto() {
 	}
 	
 	public ProfesorDto(Long id, String cedula, String nombre, int edad, String apellido, String direccion) {
-		this.id = id;
-		this.cedula = cedula;
-		this.nombre = nombre;
-		this.edad = edad;
-		this.apellido = apellido;
-		this.direccion = direccion;
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getCedula() {
-		return cedula;
-	}
-
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
+		super.id = id;
+		super.cedula = cedula;
+		super.nombre = nombre;
+		super.edad = edad;
+		super.apellido = apellido;
+		super.direccion = direccion;
 	}
 	
 }
