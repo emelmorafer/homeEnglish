389836b577e1faadb451dbf4d9dea4ee2dd509ed
Emel Mora Fernández - Ceiba Software <emel.mora@ceiba.com.co>
2019-10-10 09:44:52 -0500

    ‘guardado_de_los_ultimos_cambios’

--- src/main/java/com/ceiba/homeenglish/domain/Cliente.java
+++ src/main/java/com/ceiba/homeenglish/domain/Cliente.java
@@ -1,38 +1,12 @@
 package com.ceiba.homeenglish.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity(name = "Cliente")
-public class Cliente {
-	
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long id;
-	
-	@Column(name = "cedula", nullable = false)
-	public String cedula;
-	
-	@Column(name = "nombre", nullable = false)
-	public String nombre;
-	
-	@Column(name = "apellido", nullable = false)
-	public String apellido;
-	
-	@Column(name = "edad", nullable = true)
-	public int edad;
-	
-	@Column(name = "direccion", nullable = true)
-	public String direccion;
-	
+public class Cliente extends Persona{
 	
 	public Cliente() {
 		super();
 	}
-
 
 }
--- src/main/java/com/ceiba/homeenglish/domain/Profesor.java
+++ src/main/java/com/ceiba/homeenglish/domain/Profesor.java
@@ -1,38 +1,12 @@
 package com.ceiba.homeenglish.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity(name = "Profesor")
-public class Profesor {
-	
-	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long id;
-	
-	@Column(name = "cedula", nullable = false)
-	public String cedula;
-	
-	@Column(name = "nombre", nullable = false)
-	public String nombre;
-	
-	@Column(name = "apellido", nullable = false)
-	public String apellido;
-	
-	@Column(name = "edad", nullable = true)
-	public int edad;	
-	
-	@Column(name = "direccion", nullable = true)
-	public String direccion;
-	
-	
+public class Profesor extends Persona{
+
 	public Profesor() {
 		super();
 	}
-
 	
 }
